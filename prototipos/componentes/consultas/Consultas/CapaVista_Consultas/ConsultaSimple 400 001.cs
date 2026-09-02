77a438abccb8e9a51ef69b59cae77063ed5e1a4b
Diego Santizo <df.santizo7@gmail.com>
2026-09-01 19:41:32 -0600

    feat: agregar esqueleto para el prototipo no funcional del componente Consultas

---
+++ prototipos/componentes/consultas/Consultas/CapaVista_Consultas/ConsultaSimple 400 001.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista_Consultas
+{
+    public partial class ConsultaSimple_400_001 : Form
+    {
+        public ConsultaSimple_400_001()
+        {
+            InitializeComponent();
+        }
+    }
+}
