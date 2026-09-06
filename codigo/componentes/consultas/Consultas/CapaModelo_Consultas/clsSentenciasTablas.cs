a53eb6b1cf21a15c0bbb2d09d3e67266d35e75ca
RadeonHeartz <carriazal@miumg.edu.gt>
2026-09-06 15:38:04 -0600

    Feat: Clase de consultas agregada a la capa modelo

---
+++ codigo/componentes/consultas/Consultas/CapaModelo_Consultas/clsSentenciasTablas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CapaModelo_Consultas { 
+    public class clsSentenciasTablas { 
+        Conexion con = new Conexion(); 
+        public OdbcDataAdapter OBtenerTabla(string Tabla) 
+        { string consulta = "SELECT * FROM " + Tabla + ";"; 
+            Console.WriteLine(consulta); 
+            OdbcDataAdapter daTabla = new OdbcDataAdapter(consulta, con.conexion()); return daTabla; 
+        } 
+        public OdbcDataAdapter GetTables() { 
+            string consulta = "SHOW TABLES;"; 
+            OdbcDataAdapter daTablas = new OdbcDataAdapter(consulta, con.conexion()); 
+            return daTablas; 
+        } 
+    } 
+}
