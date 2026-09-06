16b26d2108e99440fc895d0cc8d1891ed5cc465b
RadeonHeartz <carriazal@miumg.edu.gt>
2026-09-06 15:48:28 -0600

    Feat: Clase de tablas para uso de los dgv agregada a la capa controlador

---
+++ codigo/componentes/consultas/Consultas/CapaControlador_Consultas/clsTablas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo_Consultas;
+
+namespace CapaControlador_Consultas
+{
+    public class clsTablas
+    {
+        clsSentenciasTablas sentencias = new clsSentenciasTablas();
+
+        public DataTable LlenarTabla (string NombreTabla)
+        {
+            OdbcDataAdapter daLlenarTablas = sentencias.OBtenerTabla(NombreTabla);
+            DataTable dtLlenarTablas = new DataTable();
+            daLlenarTablas.Fill(dtLlenarTablas);
+            return dtLlenarTablas;
+        }
+        public DataTable ObtenerTablas()
+        {
+            OdbcDataAdapter daObtenerTablas = sentencias.GetTables();
+            DataTable dtObtenerTablas = new DataTable();
+            daObtenerTablas.Fill(dtObtenerTablas);
+            return dtObtenerTablas;
+        }
+    }
+}
