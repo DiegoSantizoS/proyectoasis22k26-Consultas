56e3dc2786d96c8cf321ff259a71a96995907044
RadeonHeartz <carriazal@miumg.edu.gt>
2026-09-06 19:47:19 -0600

    Feat: Añadido método para llenar la tabla de consultas simples

--- codigo/componentes/consultas/Consultas/CapaVista_Consultas/UserControls/TablaSimple.cs
+++ codigo/componentes/consultas/Consultas/CapaVista_Consultas/UserControls/TablaSimple.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Data;
 using System.Windows.Forms;
+using CapaControlador_Consultas;
 
 namespace CapaVista_Consultas
 {
     public partial class TablaSimple : UserControl
     {
+        //Insertar nombre de la tabla a consultar
+        string tabla = "empleado"; // Reemplaza con el nombre real de la tabla
+        clsTablas tablas = new clsTablas();
         public TablaSimple()
         {
             InitializeComponent();
+            actuaizarDgvTablas(tabla);
+        }
+
+        private void actuaizarDgvTablas(string tablaSeleccionada)
+        {
+            ConsultasDgvSimples.DataSource = null;
+            DataTable dtTablas = tablas.LlenarTabla(tablaSeleccionada);
+            ConsultasDgvSimples.DataSource = dtTablas;
         }
     }
 }
