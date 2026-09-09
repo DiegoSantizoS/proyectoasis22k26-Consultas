7ba27da281d1bf2f5256842db6e97e44150d0f6c
miguelcont264 <dinoking2379@gmail.com>
2026-09-08 22:18:49 -0600

    creacion de sentencia invidic¿dual

---
+++ codigo/componentes/consultas/Consultas/CapaControlador_Consultas/clsCondicionLogica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControlador_Consultas
+{
+    public class clsCondicionLogica
+    {
+
+        public string Campo { get; set; }
+        public string Valor { get; set; }
+        public string OperadorLogico { get; set; }
+
+        public clsCondicionLogica(
+            string campo,
+            string valor,
+            string operadorLogico)
+        {
+            Campo = campo;
+            Valor = valor;
+            OperadorLogico = operadorLogico;
+        }
+
+        public string GenerarCondicion()
+        {
+            return $"{Campo} = '{Valor}'";
+        }
+
+        public string GenerarOperadorLogico()
+        {
+            return OperadorLogico;
+        }
+    }
+    
+}
