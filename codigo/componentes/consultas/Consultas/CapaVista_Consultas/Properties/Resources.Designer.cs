b6820465dfbfbbb01afe1334b8b69bf2aa3acf5a
Jose Cano <jpcanocobar06052005@gmail.com>
2026-09-15 19:39:25 -0600

    fix(consultas): navegacion correcta entre Consulta Simple y Compleja

--- codigo/componentes/consultas/Consultas/CapaVista_Consultas/Properties/Resources.Designer.cs
+++ codigo/componentes/consultas/Consultas/CapaVista_Consultas/Properties/Resources.Designer.cs
@@ -63,9 +63,29 @@ namespace CapaVista_Consultas.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap btn_inicio {
+            get {
+                object obj = ResourceManager.GetObject("btn_inicio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap btn_salir {
             get {
                 object obj = ResourceManager.GetObject("btn_salir", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap btn_salir1 {
+            get {
+                object obj = ResourceManager.GetObject("btn_salir1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
