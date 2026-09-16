d920f9175616d1e3a518085864acbe665d752e05
RadeonHeartz <carriazal@miumg.edu.gt>
2026-09-16 16:35:37 -0600

    feat: funcionalidad para consutas reutilizables añadida al user control UcSeleccioneUnaConsulta

--- codigo/componentes/consultas/Consultas/CapaVista_Consultas/FrmConsultasComplejas.cs
+++ codigo/componentes/consultas/Consultas/CapaVista_Consultas/FrmConsultasComplejas.cs
@@ -6,22 +6,27 @@ namespace CapaVista_Consultas
     public partial class FrmConsultasComplejas : Componentes.ClsBaseTerminus
     {
         public string TablaActual { get; set; } 
-        public FrmConsultasComplejas()
-        {
-            InitializeComponent();
-            ConsultasUcTabla.ConsultasMetAjustarAlturaFilas(30);
-        }
         public FrmConsultasComplejas(string Tabla)
         {
             InitializeComponent();
-           
+            ConsultasUcTabla.ConsultasMetAjustarAlturaFilas(30);
             TablaActual = Tabla;
-
+            ConsultasUcTabla.ConsultasProcCambiarRegistrosPorPagina(30, TablaActual);
+            ConsultasUcTabla.ConsultasProcCambiarRegistrosPorPagina(30, TablaActual);
+            ConsultasUcConsultasReutilizables.ConsultaSeleccionada += EjecutarConsultaSeleccionada;
 
         }
 
         // Inicio de código de "José Pablo Cano Cóbar" - carné: "0901-23-1727" - Fecha: "16/09/26"
-        
+
+        private void EjecutarConsultaSeleccionada(
+            string Query,
+            string Tabla)
+        {
+            ConsultasUcTabla.ConsultasProcCargarConsultaDesdeQuery(
+                Query,
+                Tabla);
+        }
         private void ConsultasBtnSalir_Click_1(object sender, EventArgs e)
         {
             DialogResult Respuesta = System.Windows.Forms.MessageBox.Show(
