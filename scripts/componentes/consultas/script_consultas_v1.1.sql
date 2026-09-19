0ad2d454d96ace3644938d95f46aebd57b855def
Diego Santizo <df.santizo7@gmail.com>
2026-09-18 19:48:17 -0600

    feat: agregado script v.1.1 de .sql y otro .sql para pruebas

--- scripts/componentes/consultas/script_consultas_v1.1.sql
+++ scripts/componentes/consultas/script_consultas_v1.1.sql
@@ -2,13 +2,14 @@ CREATE DATABASE dbConsulta;
 USE dbConsulta;
 
 CREATE TABLE tblConsulta (
-    Pk_Consulta INT AUTO_INCREMENT PRIMARY KEY,
+    Pk_Consulta INT AUTO_INCREMENT,
     nombreConsulta VARCHAR(100) NOT NULL,
-    aplicacionConsulta VARCHAR(30) NOT NULL,
+    tablaConsulta VARCHAR(255) NOT NULL, 
     queryConsulta TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-        ON UPDATE CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT Pk_Consulta PRIMARY KEY (Pk_Consulta),
+    CONSTRAINT Uq_NombreConsulta UNIQUE (nombreConsulta)
 );
-
-
