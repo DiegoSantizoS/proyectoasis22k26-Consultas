59bc822634982f7dd340f2ce76eae25912db4b66
pgomezv1 <pgomezv1@miumg.edu.gt>
2026-09-20 21:41:46 -0600

    prueba

--- codigo/componentes/consultas/Consultas/CapaControlador_Consultas/ClsTablaSeleccionada.cs
+++ codigo/componentes/consultas/Consultas/CapaControlador_Consultas/ClsTablaSeleccionada.cs
@@ -7,6 +7,15 @@ namespace CapaControlador_Consultas
         private static string _NombreTabla;
         // Inicio de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
         private static string[] _ArregloTablas;
+
+        public static void ConsultasMetGuardarTablas(string[] Tablas)
+        {
+            if (Tablas == null || Tablas.Length == 0)
+            {
+                throw new ArgumentException("El arreglo de tablas no puede estar vacío.");
+            }
+            _ArregloTablas = Tablas;
+        }
         // Fin de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
 
         public static void ConsultasMetGuardarTabla(string NombreTabla)
@@ -18,38 +27,20 @@ namespace CapaControlador_Consultas
             }
 
             _NombreTabla = NombreTabla;
-            // Inicio de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
-            switch (NombreTabla)
-            {
-                case "tblConsulta":
-                    _ArregloTablas = new string[] { "tblConsulta", "vwDetalleCompras", "vwComprasPorProveedor", "vwComprasPorProducto" };
-                    break;
-                case "tbl_rrhh_empleados":
-                    _ArregloTablas = new string[] { "tbl_rrhh_empleados", "tbl_rrhh_asistencias", "tbl_rrhh_nominas" };
-                    break;
-                case "tbl_bancos":
-                    _ArregloTablas = new string[] { "tbl_bancos", "tbl_bancos_cuentas", "tbl_bancos_transacciones" };
-                    break;
-                case "tbl_comercial_clientes":
-                    _ArregloTablas = new string[] { "tbl_comercial_clientes", "tbl_comercial_ventas", "tbl_comercial_cotizaciones" };
-                    break;
-                default:
-                    _ArregloTablas = new string[] { NombreTabla };
-                    break;
-            }
-            // Fin de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
         }
 
         public static string ConsultasFuncObtenerTabla()
         {
             return _NombreTabla;
         }
+
         // Inicio de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
         public static string[] ConsultasFuncObtenerArreglo()
         {
             return _ArregloTablas;
         }
         // Fin de código de "Pedro José Gómez Villalobos" - carné: "0901-23-4868" - Fecha: "20/09/26"
+
         public static void ConsultasMetLimpiarTabla()
         {
             _NombreTabla = null;
